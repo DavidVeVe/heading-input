b5c94ef48b8ffdcee32dd1f422e8c489939b8df8
David Velazquez <davidvelazquez@Davids-MacBook-Pro.local>
2023-01-27 16:11:01 -0600

    Add selected item and click event

--- index.js
+++ index.js
@@ -3,11 +3,12 @@ const dropdown = document.querySelector(".heading-input__dropdown");
 const dropdownContent = document.querySelector(
   ".heading-input__dropdown__content"
 );
+const documentBody = document.querySelector("body");
 
 let selectedItem;
 
-function toggleHeadingInputDropdown() {
-  headingInput.value.charAt(0) === "/"
+function toggleHeadingInputDropdown(keycode) {
+  headingInput.value.match(/\/[A-Za-z0-9]+/) && keycode !== "Escape"
     ? dropdown.classList.remove("display-none")
     : dropdown.classList.add("display-none");
 }
@@ -18,14 +19,11 @@ async function fetchDropdownData() {
   return data;
 }
 
-
-const test = () => {
-    console.log('hello')
-}
-
 function createDropdownElements(data) {
   return data.map(({ title, shortcutText, isSelected }) => {
-    return `<div onclick="test()" class="heading-input__dropdown__content__item" is-selected="${isSelected || false}">
+    return `<div class="heading-input__dropdown__content__item" is-selected="${
+      isSelected || false
+    }">
                     <h6>${title}</h6>
                     <p>Shortcut: ${shortcutText}</p>
                 </div>`;
@@ -38,18 +36,27 @@ function filterDropdownData(data) {
   });
 }
 
-headingInput.addEventListener("keyup", async () => {
+headingInput.addEventListener("keyup", async (e) => {
   const dropdownData = await fetchDropdownData();
   const filteredData = [...filterDropdownData(dropdownData)];
 
-  toggleHeadingInputDropdown();
+  toggleHeadingInputDropdown(e.code);
 
-  if(filteredData.length > 0) filteredData[0].isSelected = true;
-
+  if (filteredData.length > 0) filteredData[0].isSelected = true;
 
   dropdownContent.innerHTML = createDropdownElements(filteredData).join("");
+
+  selectedItem = document.querySelector('div[is-selected="true"]');
+  selectedItem &&
+    selectedItem.addEventListener("click", () => {
+      console.log("clicked");
+    });
 });
 
 window.addEventListener("keyup", (event) => {
   event.code === "Escape" && dropdown.classList.add("display-none");
+});
+
+documentBody.addEventListener("click", () => {
+  dropdown.classList.add("display-none");
 });
