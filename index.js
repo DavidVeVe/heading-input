ca5cc1c22511e38db0b25a318b7adb9060e5e033
David Velazquez <davidvelazquez@Davids-MacBook-Pro.local>
2023-01-26 22:40:50 -0600

    Add dropdown content

--- index.js
+++ index.js
@@ -1,6 +1,34 @@
-const headingInput = document.querySelector('#heading-input')
-const dropdown = document.querySelector('.heading-input__dropdown')
+const headingInput = document.querySelector("#heading-input");
+const dropdown = document.querySelector(".heading-input__dropdown");
+const dropdownContent = document.querySelector('.heading-input__dropdown__content')
+let dropdownData = [];
 
-headingInput.addEventListener('keyup', (event) => {
-    headingInput.value === '/1' ? dropdown.classList.remove('display-none') : dropdown.classList.add('display-none')
-})
+const toggleHeadingInputDropdown = () => {
+  headingInput.value === "/"
+    ? dropdown.classList.remove("display-none")
+    : dropdown.classList.add("display-none");
+};
+
+const fetchDropdownData = async () => {
+  const response = await fetch("./mock-data.json");
+  const data = await response.json();
+  return data;
+};
+
+const createDropdownElements = (data) => {
+    return data.map(({title, shortcut}) => {
+        return `<div><h6>${title}</h6><p>Shortcut: ${shortcut}</p></div>`
+    
+
+    });
+}
+
+headingInput.addEventListener("keyup", async () => {
+  toggleHeadingInputDropdown();
+  if (headingInput.value === "/") {
+    dropdownData = await fetchDropdownData();
+  }
+
+  dropdownContent.innerHTML = createDropdownElements(dropdownData).join('')
+
+});
