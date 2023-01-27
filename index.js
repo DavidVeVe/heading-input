435cc23c94ccecb7e58e46bf46dad60b0a791659
David Velazquez <davidvelazquez@Davids-MacBook-Pro.local>
2023-01-27 01:31:22 -0600

    Add regex for filter

--- index.js
+++ index.js
@@ -5,32 +5,53 @@ const dropdownContent = document.querySelector(
 );
 let dropdownData = [];
 
-const toggleHeadingInputDropdown = () => {
-  headingInput.value === "/"
-    ? dropdown.classList.remove("display-none")
-    : dropdown.classList.add("display-none");
-};
+function toggleHeadingInputDropdown() {
+  //headingInput.value === "/"
+    //? dropdown.classList.remove("display-none")
+    //: dropdown.classList.add("display-none");
+}
 
-const fetchDropdownData = async () => {
+async function fetchDropdownData() {
   const response = await fetch("./mock-data.json");
   const data = await response.json();
   return data;
-};
+}
 
-const createDropdownElements = (data) => {
+function createDropdownElements(data) {
   return data.map(({ title, shortcut }) => {
     return `<div>
                     <h6>${title}</h6>
                     <p>Shortcut: ${shortcut}</p>
                 </div>`;
   });
-};
+}
+
+function filterDropdownData(data) {
+    console.log('called')
+  const regex = /([A-Za-z0-9]+( [A-Za-z0-9]+)+)/i;
+  console.log(headingInput.value.slice(1))
+
+ const test = data.filter((element) => {
+    return RegExp(headingInput.value.slice(1), "gi").test(element.title)
+  });
+
+  console.log(test)
+
+  return test
+}
+
+//function useRegex(input) {
+//    let regex = /.*/i;
+//    console.log(regex.test(input));
+//    return regex.test(input);
+//}
+
+//useRegex('a')
 
 headingInput.addEventListener("keyup", async () => {
   toggleHeadingInputDropdown();
-  if (headingInput.value === "/") {
-    dropdownData = await fetchDropdownData();
-  }
+  dropdownData = await fetchDropdownData();
+    const filteredData =  filterDropdownData(dropdownData);
 
-  dropdownContent.innerHTML = createDropdownElements(dropdownData).join("");
+  dropdownContent.innerHTML = createDropdownElements(filteredData).join("");
 });
