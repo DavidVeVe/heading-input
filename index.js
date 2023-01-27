309dcfbd4f6c9223a3d2b5c3ddbd6812c7ab5233
David Velazquez <davidvelazquez@Davids-MacBook-Pro.local>
2023-01-27 02:00:53 -0600

    Fix toggleDropdown condition

--- index.js
+++ index.js
@@ -3,12 +3,11 @@ const dropdown = document.querySelector(".heading-input__dropdown");
 const dropdownContent = document.querySelector(
   ".heading-input__dropdown__content"
 );
-let dropdownData = [];
 
 function toggleHeadingInputDropdown() {
-  //headingInput.value === "/"
-    //? dropdown.classList.remove("display-none")
-    //: dropdown.classList.add("display-none");
+    headingInput.value.charAt(0) === "/"
+    ? dropdown.classList.remove("display-none")
+    : dropdown.classList.add("display-none");
 }
 
 async function fetchDropdownData() {
@@ -27,31 +26,14 @@ function createDropdownElements(data) {
 }
 
 function filterDropdownData(data) {
-    console.log('called')
-  const regex = /([A-Za-z0-9]+( [A-Za-z0-9]+)+)/i;
-  console.log(headingInput.value.slice(1))
-
- const test = data.filter((element) => {
-    return RegExp(headingInput.value.slice(1), "gi").test(element.title)
+  return data.filter((element) => {
+    return RegExp(headingInput.value.slice(1), "gi").test(element.title);
   });
-
-  console.log(test)
-
-  return test
 }
-
-//function useRegex(input) {
-//    let regex = /.*/i;
-//    console.log(regex.test(input));
-//    return regex.test(input);
-//}
-
-//useRegex('a')
 
 headingInput.addEventListener("keyup", async () => {
   toggleHeadingInputDropdown();
-  dropdownData = await fetchDropdownData();
-    const filteredData =  filterDropdownData(dropdownData);
-
+  const dropdownData = await fetchDropdownData();
+  const filteredData = filterDropdownData(dropdownData);
   dropdownContent.innerHTML = createDropdownElements(filteredData).join("");
 });
